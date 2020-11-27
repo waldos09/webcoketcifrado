33fe53c8b685e62cfe4d4e587b505cfbd6f08051
waldos09 <legos567@gmail.com>
2020-11-27 02:35:32 -0600

    chat ya cifradito

--- web_socket/index.js
+++ web_socket/index.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-
+const NodeRSA = require('node-rsa');
 
 //config del puerto
+
 app.set('port', process.env.PORT || 3000);
 
 //iniciar servidor
@@ -14,10 +15,15 @@ const server = app.listen(app.get('port'), () => {
 //enviar archivos estaticos (public)
 app.use(express.static(path.join(__dirname, 'public')));
 
+//cifrado rsa
+const key = new NodeRSA({ b: 1024 });
+//var encryptedString = key.encrypt(str, 'base64');
+//const decrypted = key.decrypt(encryptedString, 'utf8');
+
+
 //websocket
 const SocketIO = require('socket.io');
 const io = SocketIO(server);
-
 io.on('connection', (socket) => {
     console.log('new connection', socket.id);
 
@@ -25,9 +31,23 @@ io.on('connection', (socket) => {
         console.log(data)
         io.sockets.emit('chat:message', data);
     });
+
     socket.on('chat:typing', (data) => {
-        console.log(data);
         socket.broadcast.emit('chat:typing', data);
-    })
+    });
+
+    socket.on('message:cry', (data) => {
+
+        crypt = key.encrypt(data, 'base64');
+        console.log('El mensaje cifrado es; ', crypt);
+        io.sockets.emit('message:cry', crypt);
+    });
+
+    socket.on('message:decry', (data) => {
+        decrypt = key.decrypt(data, 'utf8');
+        socket.broadcast.emit('message:decry', decrypt);
+        console.log('El mensaje descifrado es: ', decrypt);
+
+    });
 
 });
--- web_socket/public/chat.js
+++ web_socket/public/chat.js
@@ -1,12 +1,18 @@
+//const { Socket } = require('socket.io');
 const socket = io()
 
 //elementos del dom (html)
 let message = document.getElementById('message');
 let username = document.getElementById('username');
 let btn = document.getElementById('send');
+let btn2 = document.getElementById('damelaCrypto');
 let output = document.getElementById('output');
 let actions = document.getElementById('actions');
+let output2 = document.getElementById('output2');
+let cryp = document.getElementById('cryp');
 
+
+//boton para enviar datos com obj
 btn.addEventListener('click', function() {
     socket.emit('chat:message', {
         message: message.value,
@@ -15,21 +21,36 @@ btn.addEventListener('click', function() {
     console.log(username.value, message.value);
 });
 
+//para saber cuando estan escribiendo
 message.addEventListener('keypress', function() {
     socket.emit('chat:typing', username.value);
-})
+});
 
+//pasar el mensaje
+btn2.addEventListener('click', function() {
+    socket.emit('message:cry', message.value);
+    console.log(cryp);
+});
+
+//pedir los datos del servidor
 socket.on('chat:message', function(data) {
     output.innerHTML += `<p>
         <strong>${data.username}</strong>: ${data.message}
-
     </p>`
-
 });
-
+//pedir quien esta ecribiendo
 socket.on('chat:typing', function(data) {
     actions.innerHTML = '';
     actions.innerHTML = `<p><em>${data} esta escribiendo</em></p>`
+});
 
+//pedir el crypt
+socket.on('message:cry', function(data) {
+    output2.innerHTML += `<input type="text" id="cryp" placeholder="clave" readonly="readonly" value="${data}"></input>`
+    console.log('El cifrado es: ', data);
+    socket.emit('message:decry', data);
+});
 
+socket.on('message:decry', function(data) {
+    console.log('El descifrado es: ', data);
 });
